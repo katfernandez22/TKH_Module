1723bf6f43c00a6df61416d97f67b998e86e9e83
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-08-18 00:38:41 -0400

    Add files via upload

---
+++ Module_5_Programming1/coding_challenges/Challenge 1/Challenge 1.js
@@ -0,0 +1,11 @@
+let fullNames = ["Westly Snipes", "Nicholas Cage", "Nasir Jones", "Sean Carter", "Sean Combs",
+"Michael Jordan", "Patrick Ewing"];
+
+let firstNames = [];
+
+let lastNames = [];
+
+for(let i = 0; i<fullNames.length; i++){
+  console.log(fullNames[i])
+
+}
